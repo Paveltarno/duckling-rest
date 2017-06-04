3e7ea0b8899d0515e45175f05d75bc65aec8515f
Pavel <paveltarno@gmail.com>
2017-06-04 18:17:47 +0300

    bump duckling version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject duckling-rest "0.1.5"
+(defproject duckling-rest "0.1.6"
   ; ...project settings...
 
   :plugins [[lein-ring "0.8.10"]]
 
   :dependencies [
 	  [org.clojure/clojure "1.8.0"]
-	  [wit/duckling "0.4.24-HE-2"]
+	  [wit/duckling "0.4.24-HE-3"]
 	  [ring/ring-core "1.2.1"]
 	  [ring/ring-jetty-adapter "1.2.1"]
 	  [compojure "1.1.6"]
